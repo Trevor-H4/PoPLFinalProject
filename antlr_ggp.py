1a17e10ebe63fa4f533b8e8bd9c8c1cce0bd63ff
Trevor-H4 <60674670+Trevor-H4@users.noreply.github.com>
2022-11-11 16:24:05 -0600

    initial file to create parser based on antlr documentation/sample in class

---
+++ antlr_ggp.py
@@ -0,0 +1,14 @@
+import sys
+from antlr4 import *
+from GeniusGentlemenParsingLexer import GeniusGentlemenParsingLexer
+from GeniusGentlemenParsingParser import GeniusGentlemenParsingParser
+ 
+def main(argv):
+    input_stream = FileStream(argv[1])
+    lexer = GeniusGentlemenParsingLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = GeniusGentlemenParsingParser(stream)
+    tree = parser.startRule()
+ 
+if __name__ == '__main__':
+    main(sys.argv)
