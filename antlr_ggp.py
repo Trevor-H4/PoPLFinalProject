cf44554bcff77627fd567fb4dcfee792d1da5bdc
Trevor-H4 <60674670+Trevor-H4@users.noreply.github.com>
2022-11-11 21:22:54 -0600

    make it print parse data

--- antlr_ggp.py
+++ antlr_ggp.py
@@ -1,5 +1,6 @@
 import sys
 from antlr4 import *
+from antlr4.tree.Trees import Trees
 from GeniusGentlemenParsingLexer import GeniusGentlemenParsingLexer
 from GeniusGentlemenParsingParser import GeniusGentlemenParsingParser
  
@@ -9,6 +10,7 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     parser = GeniusGentlemenParsingParser(stream)
     tree = parser.start()
+    print(Trees.toStringTree(tree, None, parser))
  
 if __name__ == '__main__':
     main(sys.argv)
